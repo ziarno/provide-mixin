0042962de218cb79c3aa9d51ba07376197d239ad
ziarno7 <filip@jarno.pl>
2016-02-05 09:03:52 +0100

    remove console log

--- provide-mixin.js
+++ provide-mixin.js
@@ -1,13 +1,10 @@
 ProvideMixin = function (methodOptions) {
-  var provideData;
-
-  console.log('ProvideMixin');
 
   if (_.isFunction(methodOptions.provide)) {
     methodOptions.run = _.wrap(methodOptions.run,
       function (originalRun) {
         var args = Array.prototype.slice.call(arguments, 1);
-        provideData = methodOptions.provide.apply(this, args);
+        var provideData = methodOptions.provide.apply(this, args);
         return originalRun.apply(this, args.concat([provideData]));
       }
     );
